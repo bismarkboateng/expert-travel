b10de2a46b643d83083177abd4eabdb67ea475ef
bismarkboateng <bismarkb609@gmail.com>
2023-07-24 19:29:49 +0000

    footer section component

---
+++ src/components/Footer/Data.js
@@ -0,0 +1,38 @@
+export const Company = [
+    { title: "About", index: 0 },
+    { title: "Jobs", index: 1 },
+    { title: "List your property", index: 2 },
+    { title: "Partnerships", index: 3 },
+    { title: "Investor Relations", index: 4 },
+    { title: "Advertising", index: 5 },
+    { title: "Affiliate Marketing", index: 6 },
+    { title: "Feedback", index: 7 }
+]
+
+export const Explore = [
+    { title: "United States of America travel guide", index: 0 },
+    { title: "Hotels in United States of America", index: 1 },
+    { title: "Vacation rentals in United States of America", index: 2 },
+    { title: "Vacation packages in United States of America", index: 3 },
+    { title: "Domestic flights", index: 4 },
+    { title: "Car rentals in United States of America", index: 5 },
+    { title: "All accomodation types", index: 6 },   
+]
+
+export const Policies = [
+    { title: "Privacy", index: 0},
+    { title: "Terms of use", index: 1},
+    { title: "Vrbo terms and conditions", index: 2},
+    { title: "Get started with One Key", index: 3},
+    { title: "Accessbility", index: 4},
+    { title: "Your Privacy Choices", index: 5},
+]
+
+export const Help = [
+    { title: "Support", index: 0},
+    { title: "Cancel your hotel or vacation rental booking", index: 1},
+    { title: "Cancel your flight", index: 2},
+    { title: "Refund timelines, policies & processes", index: 3},
+    { title: "Use an Expedia Coupon", index: 4},
+    { title: "International travel documents", index: 5},
+]
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,41 @@
+import styles from "./Footer.module.css";
+import { Company, Explore, Policies, Help } from "./Data";
+
+
 export default function index() {
     return (
-        <section>
-            Footer Component
+        <section className={styles.expertFooterWrapper}>
+            <h1>expedia group</h1>
+            <section className={styles.expertFooterLinksWrapper}>
+                <section className={styles.expertFooterLinks}>
+                    <p>Company</p>
+                    { Company.map((item) => (
+                        <li key={item.index}>{ item.title }</li>
+                    ))}
+                </section>
+
+                <section className={styles.expertFooterLinks}>
+                    <p>Explore</p>
+                    { Explore.map((item) => (
+                        <li className={styles.link} key={item.index}>{ item.title }</li>
+                    ))}
+                </section>
+
+                <section className={styles.expertFooterLinks}>
+                    <p>Policies</p>
+                    { Policies.map((item) => (
+                        <li key={item.index}>{ item.title }</li>
+                    ))}
+                </section>
+
+                <section className={styles.expertFooterLinks}>
+                    <p>Help</p>
+                    { Help.map((item) => (
+                        <li key={item.index}>{ item.title }</li>
+                    ))}
+                </section>
+            </section>
+           
         </section>
     )
 }
