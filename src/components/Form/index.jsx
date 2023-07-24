f9aa61656cafcb5a953eef30e659cee3fd1fdf91
bismarkboateng <bismarkb609@gmail.com>
2023-07-24 15:36:43 +0000

    implement Search component

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,54 @@
+import styles from "./Form.module.css";
+import { useState } from "react";
+import { MdLocationOn } from "react-icons/md";
+
+
+export default function index() {
+    const[inputFields, setInputFields] = useState({
+        leaving: "",
+        going: "",
+        dates: "",
+        travelers: ""
+    })
+
+    function handleFieldChange(event) {
+        // handle input change
+    }
+    
+    return (
+        <form className={styles.expertForm}>
+
+            <input
+                placeholder="Leaving from..."
+                onChange={() => handleFieldChange}
+                className={styles.expertInput}
+                value={inputFields.leaving}
+            />
+
+            <input
+                placeholder="Going to..."
+                onChange={() => handleFieldChange}
+                className={styles.expertInput}
+                value={inputFields.going}
+            />
+
+            <input
+                placeholder="Dates"
+                onChange={() => handleFieldChange}
+                className={styles.expertInput}
+                value={inputFields.dates}
+            />
+            
+            <input
+                placeholder="Travelers"
+                onChange={() => handleFieldChange}
+                className={styles.expertInput}
+                value={inputFields.travelers}
+            />
+
+            <button className={styles.expertButton}>
+                Search
+            </button>
+        </form>
+    )
+}
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,17 +1,43 @@
 import styles from "./Search.module.css";
+import { Form } from "..";
+import { useState } from "react";
 
 
 export default function index() {
+    const [travelChoice, setTravelChoice] = useState("")
+
+
+    console.log(travelChoice)
+
     return (
         <section className={styles.expertSearch}>
             <ul className={styles.expertUnordered}>
                 {["Stays", "Flights", "Cars", "Packages", "Things to do", "Cruises"].map((item, index) => (
-                    <li key={`${item} - ${index}`}>{ item }</li>
+                    <li  className={styles.listItem} key={`${item} - ${index}`}>{ item }</li>
                 ))}
             </ul>
 
             <section>
+                <ul className={styles.expertSelect}>
+                    {["Roundtrip", "One-way", "Multi-city"].map((item, index) => (
+                        <li  className={styles.listItem} key={`${item} - ${index}`}>{ item }</li>
+                    ))}
 
+                    <select
+                        className={styles.selectChoice}
+                        onChange={(event) => setTravelChoice(event.target.value)}
+                        value={travelChoice}
+                    >
+                        {["Economy", "First Class", "Premium Economy", "Business Class"].map((item, index) => (
+                            <option
+                                key={`${item}-${index}`} className={styles.fieldChoice}
+                            > 
+                                { item }
+                            </option>
+                        )) }
+                    </select>
+                </ul>
+                  <Form />  
             </section>
         </section>
     )
