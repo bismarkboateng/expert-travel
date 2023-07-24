6c8ecead43e94a15be2c5809bce58f554412aab5
bismarkboateng <bismarkb609@gmail.com>
2023-07-24 17:48:54 +0000

    gallery component

---
+++ src/components/Gallery/Data.js
@@ -0,0 +1,40 @@
+import { Image02, Image03, Image04, Image05, Image06 } from "../../assets"
+
+
+export const firstPart = [
+    { 
+        image: Image02,
+        subtitle: "WHERE TO NEXT?",
+        text: "Find flights that will take you to the most loved places",
+        index: 0,
+    },
+    { 
+        image: Image03,
+        subtitle: "MADE TO TRAVEL",
+        text: "Inspiration and deals for your next journey",
+        index: 1,
+    },
+    { 
+        image: Image04,
+        subtitle: "LAST-MINUTE ESCAPES",
+        text: "Escape the everyday with a spur-of-the-moment trip",
+        index: 2,
+    },
+
+]
+
+
+export const secondPart = [
+    { 
+        image: Image05,
+        subtitle: "DON'T MISS A BEAT",
+        text: "Add these top things to see and do to your summer trip",
+        index: 3,
+    },
+    { 
+        image: Image06,
+        subtitle: "A SUMMER ADVENTURE ABROAD",
+        text: "Make the most of your time off, and take that big international trip",
+        index: 4,
+    },
+]
--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,7 +1,35 @@
+import styles from "./Gallery.module.css"
+import { firstPart, secondPart } from "./Data";
+
+
 export default function index() {
     return (
-        <section>
-            Gallery Component
+        <section className={styles.expertGalleryWrapper}>
+            <section className={styles.expertGalleryItemWrapper}>
+                { firstPart.map((item) => (
+                    <li key={item.index} className={styles.expertGalleryItem}>
+                        <img src={item.image} 
+                            alt={`gallery image ${item.index}`} 
+                            className={styles.expertGalleryItemImage}
+                        />
+                        <small className={styles.expertGallerySubtitle}>{ item.subtitle }</small>
+                        <h5 className={styles.expertGalleryText}>{ item.text }</h5>
+                    </li>
+                ))}
+            </section>
+        
+            <section className={styles.expertGalleryItemWrapper}>
+            { secondPart.map((item) => (
+                    <li key={item.index} className={styles.expertGallerySecondItem}>
+                        <img src={item.image} 
+                            alt={`gallery image ${item.index}`} 
+                            className={styles.expertGallerySecondItemImage}
+                        />
+                        <small className={styles.expertGallerySubtitle}>{ item.subtitle }</small>
+                        <h5 className={styles.expertGalleryText}>{ item.text }</h5>
+                    </li>
+                ))}
+            </section>
         </section>
     )
 }
