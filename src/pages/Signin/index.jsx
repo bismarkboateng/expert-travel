5c2939854ed9785c012f644320392234066d0556
bismarkboateng <bismarkb609@gmail.com>
2023-08-07 23:52:35 +0000

    done implementing with sign in component, left with states

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,46 +1,49 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { AiFillApple } from "react-icons/ai";
 import { GrFacebook } from "react-icons/gr";
+import styles from "./Signin.module.css";
 
 
 
 
 export default function index() {
     return (
-        <section>
-            <div>Sign in or create an account</div>
-            <p>Unlock a world of rewards with one account across Expedia, <br />
+        <section className={styles.signin}>
+            <img src="https://www.expedia.com/_dms/header/logo.svg?locale=en_US&siteid=1&2&3ec60977" alt="expert travel logo" className={styles.expedia} />
+            <div className={styles.signinCreate}>Sign in or create an account</div>
+            <p className={styles.pText}>Unlock a world of rewards with one account across Expedia, <br />
                Hotels.com, and Vrbo.
             </p>
 
-            <div>
+            <div className={styles.googleBtn}>
                 <img />
                 <button>
                     Sign in with Google
                 </button>
             </div>
 
-            <form>
+            <form className={styles.siginForm}>
                 <input 
                     placeholder="Email"
+                    className={styles.signinInput}
                     type="email"
                     name="email"
                     onChange={() => {}}
                     value=""
                 />
 
-                <div>Please enter your email address.</div>
+                <div className={styles.formText}>Please enter your email address.</div>
 
-                <button>
+                <button className={styles.signinBtn}>
                     Continue
                 </button>
             </form>
 
-            <div>Other ways to sign in</div>
+            <div className={styles.signinP}>Other ways to sign in</div>
 
-            <div>
+            <div className={styles.links}>
                 <AiFillApple />
-                <GrFacebook />
+                <GrFacebook className={styles.fb}/>
             </div>
         
         </section>
